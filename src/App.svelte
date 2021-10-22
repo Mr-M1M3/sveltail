d097898ebafe02c10ce8b6c91fbc7744b9b5e634
Mr. M1M3 <abir.sheikh.4044@gmail.com>
2021-10-22 12:08:06 +0600

    initial commit :D

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+// imports css files
+import "./tailwind.css"
+	export let name;
+</script>
+
+<main>
+	<h1 class="text-blue-900">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
